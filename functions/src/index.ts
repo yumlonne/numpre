c622ffcc8f7f36eb4897e98d4a8562c12019fd76
yumlonne <yumlonne@gmail.com>
2019-04-27 04:17:42 +0900

    issue #1: 簡易的にregister実装

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,27 @@
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+import { Guid } from 'guid-typescript'
+
+admin.initializeApp();
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
+});
+
+export const register = functions.https.onRequest((request, response) => {
+    const question = request.query.question;
+    const answer = request.query.answer;
+    // FIXME: check question, answer
+
+    const uuid = Guid.create();
+    const ref  = admin.database().ref(`/question/${uuid}`);
+    ref.set({
+        "answer": answer,
+        "question": question,
+    }).catch();
+
+    response.send(`Accepted! Your Question id is ${uuid}`);
 });
